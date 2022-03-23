b14b7421341f50bf4cb4a512bf82576e7b7c3112
Arunak11 <sarunjayan14@gmail.com>
2022-03-23 15:30:26 +0530

    23-03-2022 task

---
+++ src/exception/trycatch.java
@@ -0,0 +1,22 @@
+package exception;
+
+public class trycatch {
+
+        public static void main(String[] args) {
+            try
+            {
+                String uname=null;
+                System.out.println(""+uname.length());
+            }
+            catch(NullPointerException y)
+            {
+                System.out.println(y);
+            }
+
+            finally
+            {
+                System.out.println("finally block");
+            }
+
+        }
+}
