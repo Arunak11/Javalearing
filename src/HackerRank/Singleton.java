797323d014318373bb1c9385a4fa7c511ed6f8b6
Arunak11 <sarunjayan14@gmail.com>
2022-03-30 10:38:39 +0530

    28-03-2022 task

---
+++ src/HackerRank/Singleton.java
@@ -0,0 +1,21 @@
+package HackerRank;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+public class Singleton{
+    static Singleton instance = new Singleton();
+    private Singleton() {
+
+    }
+    public String str;
+    static Singleton getSingleInstance() {
+        return instance;
+    }
+
+}
