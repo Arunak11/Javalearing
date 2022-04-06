6c702b4870cee4b61bf9370c3e73d39d0d6baa84
Arunak11 <sarunjayan14@gmail.com>
2022-04-06 16:35:41 +0530

    Completed DS

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,20 @@
 package com.company;
-import java.util.Scanner;
 
 public class Main {
-    int a[] = new int[5];
-    int i,n;
-    Scanner sc = new Scanner(System.in);
     void get1()
     {
-        System.out.println("Enter the value:");
-        n=sc.nextInt();
-        for(i=0;i<=n;i++)
-        {
-            System.out.println("Enter the elements:");
-            a[i] = sc.nextInt();
-        }
-        for(i=0;i<=n;i++)
-        {
-            System.out.println("Your value is::"+a[i]);
-        }
+        System.out.println("Welcome to first function");
+    }
+    void get2()
+    {
+        System.out.println("Welcome to second function");
     }
 
     public static void main(String[] args) {
+        System.out.println("Welcome to main class");
         Main m1 = new Main();
         m1.get1();
-
+        m1.get2();
     }
 }
+
